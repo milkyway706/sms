3d198a6de39a813a3ca5fd498320a7c4e2fb81fb
admin <admin@goragod.com>
2018-07-09 17:10:28 +0700

    format Code update Kotchasan

--- load.php
+++ load.php
@@ -3,14 +3,13 @@
  * load.php.
  *
  * @author Goragod Wiriya <admin@goragod.com>
- *
- * @see http://www.kotchasan.com/
- *
  * @copyright 2016 Goragod.com
  * @license http://www.kotchasan.com/license/
+ *
+ * @see http://www.kotchasan.com/
  */
 /*
- * Site root
+ * Site root.
  */
 define('ROOT_PATH', str_replace('\\', '/', dirname(__FILE__)).'/');
 /*
@@ -22,12 +21,16 @@ define('DATA_FOLDER', 'datas/');
  * 1 บันทึกข้อผิดพลาดและคำเตือนลง error_log .php
  * 2 แสดงผลข้อผิดพลาดและคำเตือนออกทางหน้าจอ (ใช้เฉพาะตอนออกแบบเท่านั้น)
  */
-define('DEBUG', 2);
+define('DEBUG', 0);
 /*
  * false (default)
  * true บันทึกการ query ฐานข้อมูลลง log (ใช้เฉพาะตอนออกแบบเท่านั้น)
  */
 define('DB_LOG', false);
+/*
+ * ภาษาอัตโนมัติจากบราวเซอร์
+ */
+define('AUTO_LANGUAGE', false);
 /**
  * load Kotchasan.
  */
